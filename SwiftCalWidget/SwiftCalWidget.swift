c8dcac844d8f1d793a2c897cd2258f660da85d68
Jason Mitchell <devjmitchell@gmail.com>
2024-04-09 09:39:22 -0400

    Add touch targets to widget

--- SwiftCalWidget/SwiftCalWidget.swift
+++ SwiftCalWidget/SwiftCalWidget.swift
@@ -59,35 +59,39 @@ struct SwiftCalWidgetEntryView : View {
     
     var body: some View {
         HStack {
-            VStack {
-                Text("\(calculateStreakValue())")
-                    .font(.system(size: 70, design: .rounded))
-                    .bold()
-                    .foregroundStyle(.orange)
-                
-                Text("day streak")
-                    .font(.caption)
-                    .foregroundStyle(.secondary)
+            Link(destination: URL(string: "streak")!) {
+                VStack {
+                    Text("\(calculateStreakValue())")
+                        .font(.system(size: 70, design: .rounded))
+                        .bold()
+                        .foregroundStyle(.orange)
+                    
+                    Text("day streak")
+                        .font(.caption)
+                        .foregroundStyle(.secondary)
+                }
             }
             
-            VStack {
-                CalendarHeaderView(font: .caption)
-                
-                LazyVGrid(columns: columns, spacing: 7) {
-                    ForEach(entry.days) { day in
-                        if day.date!.monthInt != Date().monthInt {
-                            Text("")
-                        } else {
-                            Text(day.date!.formatted(.dateTime.day()))
-                                .font(.caption2)
-                                .bold()
-                                .frame(maxWidth: .infinity)
-                                .foregroundStyle(day.didStudy ? .orange : .secondary)
-                                .background(
-                                    Circle()
-                                        .foregroundStyle(.orange.opacity(day.didStudy ? 0.3 : 0.0))
-                                        .scaleEffect(1.5)
-                                )
+            Link(destination: URL(string: "calendar")!) {
+                VStack {
+                    CalendarHeaderView(font: .caption)
+                    
+                    LazyVGrid(columns: columns, spacing: 7) {
+                        ForEach(entry.days) { day in
+                            if day.date!.monthInt != Date().monthInt {
+                                Text("")
+                            } else {
+                                Text(day.date!.formatted(.dateTime.day()))
+                                    .font(.caption2)
+                                    .bold()
+                                    .frame(maxWidth: .infinity)
+                                    .foregroundStyle(day.didStudy ? .orange : .secondary)
+                                    .background(
+                                        Circle()
+                                            .foregroundStyle(.orange.opacity(day.didStudy ? 0.3 : 0.0))
+                                            .scaleEffect(1.5)
+                                    )
+                            }
                         }
                     }
                 }
